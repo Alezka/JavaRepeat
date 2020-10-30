6a7675fc6a052525dc79d80e3f1beae9c6749d0b
Alezka <oleg.hoj@gmail.com>
2020-10-30 10:54:05 +0200

    switch

---
+++ src/topic_7_switch/Fighter.java
@@ -0,0 +1,22 @@
+package topic_7_switch;
+
+public class Fighter {
+    String name;
+    int power;
+
+    public Fighter() {
+    }
+
+    public Fighter(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Fighter{" +
+                "name='" + name + '\'' +
+                ", power=" + power +
+                '}';
+    }
+}
