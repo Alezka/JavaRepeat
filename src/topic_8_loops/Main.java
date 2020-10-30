b8ed5f23eb389f53705ea7849ed79a9e3f45c374
Alezka <oleg.hoj@gmail.com>
2020-10-30 10:54:34 +0200

    loops

---
+++ src/topic_8_loops/Main.java
@@ -0,0 +1,27 @@
+package topic_8_loops;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+//        int i = 0;
+//        while (i<10){
+//            System.out.println("done");
+//            i++;
+//        }
+
+//        for(;;){} безкінечний цикл
+//        for (int min = 0; min <= 60; min++) {
+//
+//            for (int sec = 0; sec < 60; sec++){
+//                System.out.println("min" + min +" sec"+sec);
+////                String arg = args[sec];
+//            }
+//
+//        }
+
+        do{
+            System.out.println("lorem");
+        } while (false);
+    }
+}
