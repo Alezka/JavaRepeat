5b3c5a66c29156e72213e2fa563ff9c5d28dc70d
Alezka <oleg.hoj@gmail.com>
2020-11-05 21:46:45 +0200

    arrays

---
+++ src/topic_9_arrays/Car.java
@@ -0,0 +1,26 @@
+package topic_9_arrays;
+
+public class Car {
+    String model;
+    double volume;
+    int power;
+
+    public Car() {
+
+    }
+
+    public Car(String model, double volume, int power) {
+        this.model = model;
+        this.volume = volume;
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", volume=" + volume +
+                ", power=" + power +
+                '}';
+    }
+}
